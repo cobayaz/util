c8b91e52543d72d3b89eed408779d08afbad357d
swnb <1786718956@qq.com>
2017-12-29 18:02:16 +0800

    添加测试的版本

---
+++ dir_info/index_async.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const path = require('path');
+const util = require('util');
+const mime = require('mime');
+
+const readDir = util.promisify(fs.readdir);
+const fileStat = util.promisify(fs.stat);
+
+const dir_info = {};
+
+const get_dir_info = (dirname, object = dir_info) => {
+    const files = fs.readdirSync(dirname);
+    for (file of files) {
+        if (file.startsWith('.')) {
+            continue;
+        }
+        const abs_path = path.join(dirname, file);
+        const stat = fs.statSync(abs_path);
+        if (stat.isFile()) {
+            const type = mime.getType(abs_path);
+            object[file] = type;
+        } else if (stat.isDirectory()) {
+            object[file] = {};
+            const obj_index = object[file];
+            get_dir_info(abs_path, obj_index);
+        } else {
+            console.log(file);
+        }
+    }
+    return dir_info;
+};
+
+module.exports = get_dir_info;
---
+++ dir_info/test/promise.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+const path = require('path');
+const util = require('util');
+const mime = require('mime');
+
+const readDir = util.promisify(fs.readdir);
+const fileStat = util.promisify(fs.stat);
+
+const dir_info = {};
+const get_dir_info = async (dirname, object = dir_info) => {
+    const files = await readDir(dirname);
+    for (file of files) {
+        if (file.startsWith('.')) {
+            continue;
+        }
+        const abs_path = path.join(dirname, file);
+        const stat = await fileStat(abs_path);
+        if (stat.isFile()) {
+            const type = mime.getType(abs_path);
+            object[file] = type;
+        } else if (stat.isDirectory()) {
+            object[file] = {};
+            const obj_index = object[file];
+            await get_dir_info(abs_path, obj_index);
+        } else {
+            console.log(file);
+        }
+    }
+    return dir_info;
+};
+async function main() {
+    console.time('pro');
+    const a = await get_dir_info('../');
+    console.timeEnd('pro');
+    console.log(a);
+}
+main();

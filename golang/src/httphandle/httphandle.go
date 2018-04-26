c175bff31b47d9c30b1e6a9feb41a9940177261a
swnb <1786718956@qq.com>
2018-04-26 19:03:06 +0800

    explain

--- golang/src/httphandle/httphandle.go
+++ golang/src/httphandle/httphandle.go
@@ -2,6 +2,7 @@ package httphandle
 
 import "net/http"
 
+//Merge merge the handle func
 func Merge(f ...http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		for _, function := range f {

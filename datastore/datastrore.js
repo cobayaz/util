53fd655950a0721dbbeb7d8f16d67359c223d9b6
swnb <1786718956@qq.com>
2018-06-02 17:40:44 +0800

    reformat datastore

--- datastore/datastrore.js
+++ datastore/datastrore.js
@@ -1,14 +1,20 @@
-class dataStore {
+const isObject = param => {
+    return param && typeof param === "object" && !Array.isArray(param)
+        ? true
+        : false;
+};
+
+class DataStore {
     constructor(obj = {}) {
         this.data = new Map();
-        for (const x in obj) {
-            this.data.set(x, obj[x]);
+        for (const attr in obj) {
+            this.data.set(attr, obj[attr]);
         }
     }
     set(keyOrObject, value) {
         if (typeof keyOrObject === "object" && !Array.isArray(keyOrObject)) {
             const object = keyOrObject;
-            for (x in object) {
+            for (const x in object) {
                 this.data.set(x, object[x]);
             }
         } else {
@@ -18,7 +24,43 @@ class dataStore {
         return this;
     }
     get(key) {
+        const val = this.data.get(key);
+        const solve = fn => {
+            const newData = fn(val);
+            if (newData) {
+                this.data.set(key, newData);
+            }
+            return this;
+        };
+        solve.solve = solve; //取 改 写
+        return solve;
+    }
+    setData(objOrFn) {
+        if (objOrFn) {
+            if (typeof objOrFn === "function") {
+                const fn = objOrFn;
+            } else {
+                const newData = objOrFn;
+            }
+        } else {
+            throw Error("not a argument");
+        }
+        if (isObject(newData)) {
+            for (const attr in newData) {
+                this.data.set(attr, newData[attr]);
+            }
+        }
+    }
+    getVal(key) {
         return this.data.get(key);
+    }
+    has(key) {
+        if (this.data.has(key)) {
+        }
+    }
+    rm(key) {
+        this.data.delete(key);
+        return this;
     }
     clear() {
         this.data.clear();
@@ -28,13 +70,52 @@ class dataStore {
         console.log(this.data.get(key));
         return this;
     }
-    static getInstance() {
-        if (dataStore.instance) {
-            return dataStore.instance;
+    printAll() {
+        console.log(this.data);
+        return this;
+    }
+    async() {}
+    timeout(key, time) {}
+    static getInstance(id = "boss") {
+        if (DataStore.instance) {
+            return DataStore.instance;
         }
-        return (dataStore.instance = dataStore.create());
+        return (DataStore.instance = DataStore.create());
     }
     static create(arg) {
-        return new dataStore(arg);
+        return new DataStore(arg);
     }
 }
+
+let data = DataStore.getInstance();
+
+data
+    .set("keyss", "valss")
+    .set({ keysss: "valsss", keysssss: "valsssss" })
+    .printAll()
+    .print("keyss")
+    .get("keyss")(data => {
+        console.log(data);
+    })
+    .get("keyss")
+    .solve(data => {
+        console.log("thisi is solve");
+    })
+    .print("keyss")
+    .set("key", "cdcd");
+
+data
+    .get("key")(function(val) {
+        console.log(val);
+        return val + "xsxs" + "xsxs";
+    })
+    .setData()
+    .print("key");
+
+data
+    .get("key")
+    .solve(function(val) {
+        console.log(val);
+        return val + "xcd" + "cd";
+    })
+    .print("key");

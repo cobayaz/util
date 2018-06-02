f06481710006317db0b9a408f55d43259d2fda15
swnb <1786718956@qq.com>
2018-06-02 18:54:18 +0800

    type resolve

---
+++ type/type.js
@@ -0,0 +1,20 @@
+const isSameType = (a, b) => {
+    if (!a) {
+        //null undefined
+        return b ? false : true;
+    } else if (Array.isArray(a)) {
+        //数组
+        return Array.isArray(b) ? true : false;
+    } else if (typeof a === typeof b) {
+        //string number bool function object
+        return true;
+    } else {
+        return false;
+    }
+};
+
+const isObject = param => {
+    return param && typeof param === "object" && !Array.isArray(param)
+        ? true
+        : false;
+};

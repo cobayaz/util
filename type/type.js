9f2bd7fe39b24817604579a1f0f80de7f50992a5
swnb <1786718956@qq.com>
2018-06-02 19:58:35 +0800

    base type

--- type/type.js
+++ type/type.js
@@ -18,3 +18,18 @@ const isObject = param => {
         ? true
         : false;
 };
+
+const isSameType = (a, b) => {
+    if (!a) {
+        //null undefined
+        return b ? false : true;
+    } else if (Array.isArray(a)) {
+        //数组
+        return Array.isArray(b) ? true : false;
+    } else if (typeof a === typeof b) {
+        //string number bool function object
+        return true;
+    } else {
+        return false;
+    }
+};
